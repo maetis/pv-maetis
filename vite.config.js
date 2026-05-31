cb812cfc2b727f01f2e2d6f98d2998950354acc7
maetis <direction@maetis-cpta.ch>
2026-05-31 17:16:29 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+      }
+    }
+  }
 });
